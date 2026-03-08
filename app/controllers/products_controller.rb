9986e9a22a8014dbad1c34e8ec1e8d354cc8a291
Theingi Lin <102965090+theingi96@users.noreply.github.com>
2026-03-08 13:33:46 +0900

    Add product creation form and create action

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,9 +1,31 @@
 class ProductsController < ApplicationController
+  before_action :authenticate_user!, only: [:new, :create]
+
   def index
     @products = Product.includes(:category, image_attachment: :blob).order(created_at: :desc)
   end
 
   def show
     @product = Product.find(params[:id])
+  end
+
+  def new
+    @product = Product.new
+  end
+
+  def create
+    @product = Product.new(product_params)
+
+    if @product.save
+      redirect_to root_path, notice: "Product was successfully created."
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def product_params
+    params.require(:product).permit(:name, :description, :price, :stock, :is_active, :category_id, :image)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
 
   root "products#index"
-  resources :products, only: [:index, :show]
+  resources :products, only: [:index, :show, :new, :create]
 end
